30589daa09923860d04de174fa78def86203733a
MagdyR <developer.eye1@gmail.com>
2026-02-26 14:26:04 +0200

    Add script section to layout for dynamic script inclusion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,5 +114,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    @yield('scripts')
 </body>
 </html>
