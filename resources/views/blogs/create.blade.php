c8df6903f1a4903305cda8fd2a4c467c373f7648
MagdyR <developer.eye1@gmail.com>
2026-02-26 13:42:24 +0200

    Add error and success message alerts to blog creation form

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 <div class="container mt-5">
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Please fix the following errors:</strong>
+            <ul class="mb-0 mt-2">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
     <div class="card">
         <div class="card-header bg-primary text-white">
             <h3>Create New Blog</h3>
