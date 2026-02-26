ce354b5216ecd99d184c24009e35f69b4d9358e3
MagdyR <developer.eye1@gmail.com>
2026-02-26 14:03:42 +0200

    Refactor blog index view by removing redundant card structure and consolidating blog item display

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -31,6 +31,15 @@
                     </div>
                     <div class="card-body">
                         <p class="mb-1">{{ $blog->excerpt }}</p>
+                        <p class="text-muted mb-1">{{ $blog->publish_date }} by {{ $blog->author }}</p>
+
+                        <a href="{{ route('blogs.edit', $blog->id) }}" class="btn btn-warning btn-sm">Edit</a>
+
+                        <form action="{{ route('blogs.destroy', $blog->id) }}" method="POST" class="d-inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -38,28 +47,4 @@
     </div>
 </div>
 
-<div class="container mt-5">
-    <div class="card">
-        <div class="card-header bg-info text-white">
-
-
-        </div>
-        <div class="card-body">
-            @foreach ($blogs as $blog)
-                <div class="blog-item mb-4 p-3 border-bottom">
-                    <h4 class="mb-2">{{ $blog->title }}</h4>
-                    <p class="mb-1">{{ $blog->excerpt }}</p>
-                    <p class="text-muted mb-1">{{ $blog->publish_date }} by {{ $blog->author }}</p>
-                    <a href="{{ route('blogs.edit', $blog->id) }}" class="btn btn-warning btn-sm">Edit</a>
-
-                    <form action="{{ route('blogs.destroy', $blog->id) }}" method="POST" class="d-inline-block">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
-                    </form>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</div>
 @endsection
