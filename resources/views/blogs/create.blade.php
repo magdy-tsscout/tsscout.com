4aa0ec7d6faa3c78a477b1ee6a63067a04c8426b
MagdyR <developer.eye1@gmail.com>
2026-04-22 14:45:58 +0200

    ff

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -117,42 +117,42 @@
         image_title: true,
         automatic_uploads: true,
         images_upload_url: "{{ route('upload-handler') }}",
-        images_upload_handler: function (blobInfo, progress) {
-            return new Promise((resolve, reject) => {
-                const xhr = new XMLHttpRequest();
-                xhr.withCredentials = false;
-                xhr.open('POST', "{{ route('upload-handler') }}");
+        // images_upload_handler: function (blobInfo, progress) {
+        //     return new Promise((resolve, reject) => {
+        //         const xhr = new XMLHttpRequest();
+        //         xhr.withCredentials = false;
+        //         xhr.open('POST', "{{ route('upload-handler') }}");
 
-                // إضافة الـ CSRF Token من الـ Meta Tag
-                const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-                xhr.setRequestHeader('X-CSRF-TOKEN', token);
+        //         // إضافة الـ CSRF Token من الـ Meta Tag
+        //         const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        //         xhr.setRequestHeader('X-CSRF-TOKEN', token);
 
-                xhr.upload.onprogress = (e) => {
-                    progress(e.loaded / e.total * 100);
-                };
+        //         xhr.upload.onprogress = (e) => {
+        //             progress(e.loaded / e.total * 100);
+        //         };
 
-                xhr.onload = () => {
-                    if (xhr.status === 403) {
-                        reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
-                        return;
-                    }
-                    if (xhr.status < 200 || xhr.status >= 300) {
-                        reject('HTTP Error: ' + xhr.status);
-                        return;
-                    }
-                    const json = JSON.parse(xhr.responseText);
-                    if (!json || typeof json.location != 'string') {
-                        reject('Invalid JSON: ' + xhr.responseText);
-                        return;
-                    }
-                    resolve(json.location);
-                };
+        //         xhr.onload = () => {
+        //             if (xhr.status === 403) {
+        //                 reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
+        //                 return;
+        //             }
+        //             if (xhr.status < 200 || xhr.status >= 300) {
+        //                 reject('HTTP Error: ' + xhr.status);
+        //                 return;
+        //             }
+        //             const json = JSON.parse(xhr.responseText);
+        //             if (!json || typeof json.location != 'string') {
+        //                 reject('Invalid JSON: ' + xhr.responseText);
+        //                 return;
+        //             }
+        //             resolve(json.location);
+        //         };
 
-                const formData = new FormData();
-                formData.append('file', blobInfo.blob(), blobInfo.filename());
-                xhr.send(formData);
-            });
-        },
+        //         const formData = new FormData();
+        //         formData.append('file', blobInfo.blob(), blobInfo.filename());
+        //         xhr.send(formData);
+        //     });
+        // },
         toolbar: 'undo redo | formatselect | bold italic backcolor | link image | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat',
         content_css: 'https://www.tiny.cloud/css/codepen.min.css'
     });
