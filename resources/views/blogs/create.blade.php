12cda4bbcb19103fa69770a5e3b0bc6baa935e66
MagdyR <developer.eye1@gmail.com>
2026-04-22 15:03:44 +0200

    Add file picker for image uploads in TinyMCE editor

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -164,6 +164,33 @@
 
         image_dimensions: true,
         image_advtab: true,
+        file_picker_callback: function (cb, value, meta) {
+            const input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+            input.onchange = function () {
+                const file = this.files[0];
+                const reader = new FileReader();
+                reader.onload = function () {
+                    /*
+                    هنا نقوم برفع الصورة مؤقتاً كـ Blob
+                    TinyMCE سيتكفل برفعه لـ Laravel عبر images_upload_url
+                    */
+                    const id = 'blobid' + (new Date()).getTime();
+                    const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    const base64 = reader.result.split(',')[1];
+                    const blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    // نضع الرابط المؤقت في خانة الـ Source
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+                reader.readAsDataURL(file);
+            };
+
+            input.click();
+        },
 
         toolbar: 'undo redo | formatselect | bold italic backcolor | link image | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat',
 
