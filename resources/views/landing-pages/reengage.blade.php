39954a60b5a7170e4b67a44b7effb38db8278661
MagdyR <developer.eye1@gmail.com>
2026-06-11 18:49:31 +0300

    Add Google Tag Manager script for enhanced tracking capabilities

--- resources/views/landing-pages/reengage.blade.php
+++ resources/views/landing-pages/reengage.blade.php
@@ -15,6 +15,13 @@
   <title>TSScout – Find Winning Products Before Your Competitors</title>
   <link rel="canonical" href="{{ url()->current() }}">
   <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/Scout-Logo%2020x20-01.svg') }}">
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KV3N43LJ');
+  </script>
 <style type="text/css">
     /* ── RESET & BASE ── */
     *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
