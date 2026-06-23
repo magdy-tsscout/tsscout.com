4ac140d3fea6666771b89bee86b69b71665f676c
MagdyR <developer.eye1@gmail.com>
2026-06-23 23:31:28 +0300

    Refactor 'id' column creation in users migration to use the 'id()' method

--- database/migrations/2026_06_23_202950_add_users_id.php
+++ database/migrations/2026_06_23_202950_add_users_id.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             if (!Schema::hasColumn('users', 'id')) {
-                $table->id();
+                $table->unsignedBigInteger('id')->primary()->first();
             }
         });
     }
