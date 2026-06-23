ce948e959be2a52806a4f5cb7b68331430b58968
MagdyR <developer.eye1@gmail.com>
2026-06-23 14:24:54 +0300

    Adjust card margin for categories display in Sellers Dictionary

--- resources/views/sellers-dictionary/web-index-home.blade.php
+++ resources/views/sellers-dictionary/web-index-home.blade.php
@@ -25,7 +25,7 @@
         {!! $content->content !!}
     </div>
     @foreach ($categories as $category)
-        <div class="card mt-4">
+        <div class="card mt-3">
             <div class="card-body">
             <h2 class="h5 p-0 m-0"><a href="{{ route('sellers-dictionary.web.index', $category->slug) }}" class="d-block">{{ $category->name }}</a></h2>
             </div>
