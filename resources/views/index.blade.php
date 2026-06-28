4dfd99e6cb907ec439b8b162cb4a809cd4e0b337
MagdyR <developer.eye1@gmail.com>
2026-06-28 15:04:28 +0300

    Enhance schema markup with social media links and detailed WebApplication information for improved SEO

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -743,12 +743,29 @@
 
 @push('schema')
     <script type="application/ld+json">
-        {
+    {
         "@@context": "https://schema.org",
         "@@type": "Organization",
         "name": "TS Scout",
         "url": "{{ url("/") }}",
-        "logo": "{{ url('/images/logo.svg') }}"
-        }
+        "logo": "{{ url('/images/logo.svg') }}",
+        "sameAs": [
+            "https://www.facebook.com/tsscout",
+            "https://twitter.com/tsscout"
+        ]
+    },
+    {
+      "@@type": "WebApplication",
+      "@@id": "https://tsscout.com/#webapp",
+      "url": "https://tsscout.com",
+      "name": "TS Scout",
+      "applicationCategory": "BusinessApplication",
+      "operatingSystem": "All",
+      "browserRequirements": "Requires HTML5 support",
+      "description": "An all-in-one eCommerce intelligence platform designed for product research, competitor monitoring, and market analytics.",
+      "publisher": {
+        "@id": "https://tsscout.com/#organization"
+      }
+    }
     </script>
 @endpush
