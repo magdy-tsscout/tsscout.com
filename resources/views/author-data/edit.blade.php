16bff0a1d8c794a94151a0ceb10d340f68ff34eb
MagdyR <developer.eye1@gmail.com>
2026-06-28 18:53:22 +0300

    ff

--- resources/views/author-data/edit.blade.php
+++ resources/views/author-data/edit.blade.php
@@ -6,22 +6,22 @@
     @method('POST')
    <div class="form-group">
         <label for="author_name">Author Name</label>
-        <input type="text" class="form-control" id="author_name" name="author_name" value="{{ old('author_name', $authorData->author_name) }}">
+        <input type="text" class="form-control" id="author_name" name="author_name" value="{{ old('author_name', $user->author_name) }}">
     </div>
     <div class="form-group">
         <label for="author_card">Author about</label>
-        <textarea class="form-control" id="author_card" name="author_card">{{ old('author_card', $authorData->author_card) }}</textarea>
+        <textarea class="form-control" id="author_card" name="author_card">{{ old('author_card', $user->author_card) }}</textarea>
     </div>
     <div class="form-group">
         <label for="author_slug">Author Slug</label>
-        <input type="text" class="form-control" id="author_slug" name="author_slug" value="{{ old('author_slug', $authorData->author_slug) }}">
+        <input type="text" class="form-control" id="author_slug" name="author_slug" value="{{ old('author_slug', $user->author_slug) }}">
     </div>
     <div class="form-group">
         <label for="author_img">Author Image</label>
         <input type="file" class="form-control" id="author_img" name="author_img">
         <div class="mt-2">
-            @if($authorData->author_img)
-                <img src="{{ asset('images/' . $authorData->author_img) }}" alt="Author Image" class="img-thumbnail" style="max-width: 200px;">
+            @if($user->author_img)
+                <img src="{{ asset('images/' . $user->author_img) }}" alt="Author Image" class="img-thumbnail" style="max-width: 200px;">
             @endif
         </div>
         <div class="mt-2">
