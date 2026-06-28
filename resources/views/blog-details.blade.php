e0ed7253f6eeb14c82e10040cf7d2fa95828b4b0
MagdyR <developer.eye1@gmail.com>
2026-06-28 15:31:55 +0300

    Enhance blog details view with improved layout and structured data for SEO

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -279,214 +279,245 @@
 @endsection
 
 @section('content')
-@if( !$blog->published && Auth::check() )
-    <div class="alert alert-warning text-center" style="margin-right: -15px;margin-left: -15px;" role="alert">
-        This blog is currently in draft mode and not visible to the public.
-        <a href="{{ route('blogs.edit', $blog->id) }}" class="btn btn-sm btn-warning">Edit Blog</a>
-    </div>
-@endif
-<div class="container blog-details-container">
-    <a href="{{ route('blogs.userIndex') }}"><img src="{{ asset('images/left-arrow.svg') }}" class="back-arrow" alt="Back"></a>
-    <h1 class="title">{{ $blog->title }}</h1>
-    <div class="rowElements">
-        <button class="button-shopify">{{ $blog->category }}</button>
-        <ul class="info-list">
-            <li><span class="date">{{ $blog->publish_date }}</span></li>
-            {{-- <li><span class="author">By: {{ $blog->author }}</span></li> --}}
-            <li>
-                <img id="" src="{{ asset('images/heart.svg') }}" alt="Likes" >
-                <span id="like-count" class="number">{{ $blog->likes }}</span>
-            </li>
-        </ul>
-    </div>
-
-
-             @if ($blog->video_url)
-                <iframe width="100%" height="415" src="https://www.youtube.com/embed/{{ \Str::after($blog->video_url, 'v=') }}" frameborder="0" allowfullscreen></iframe>
-            @else
-                <a href="{{ route('blogs.show', $blog->slug) }}">
-                    <img src="{{ 'https://tsscout.com/storage/app/public/' .$blog->image }}" alt="{{ $blog->title }}">
-                </a>
-            @endif
-
-
-    <div class="content-container">
-        <div class="content">
-            {!! $blog->content !!}
-            <br>
-            like?
-            <img id="like-button" src="{{ asset('images/heart.svg') }}" alt="Likes" style="cursor: pointer;">
-
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-3">
-                            <a href="{{ route('author.show', $blog->author_data->author_slug) }}"><img src="{{ $blog->author_data?->author_img ? asset('images/'.$blog->author_data->author_img) : asset('images/icon-client.svg') }}" alt="{{ $blog->author_data->author_name }}" class="img-fluid rounded"></a>
-                        </div>
-                        <div class="col-lg-9">
-                            <a href="{{ route('author.show', $blog->author_data->author_slug) }}">
-                            <h4>{{ $blog->author_data->author_name }}</h4>
-                            <p>{!! nl2br($blog->author_data->author_card) !!}</p>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    @if( !$blog->published && Auth::check() )
+        <div class="alert alert-warning text-center" style="margin-right: -15px;margin-left: -15px;" role="alert">
+            This blog is currently in draft mode and not visible to the public.
+            <a href="{{ route('blogs.edit', $blog->id) }}" class="btn btn-sm btn-warning">Edit Blog</a>
         </div>
-        <div class="sidebar">
-            <div class="social">
-                <ul>
-                    <li><a href="https://www.tiktok.com/@dropshipping.scout" target="_blank"><img src="{{ asset('images/Tiktok.svg') }}" alt=""></a></li>
-                    {{-- <li><a href="#"><img src="{{ asset('images/Whatsapp.svg') }}" alt=""></a></li> --}}
-                    <li><a href="https://www.linkedin.com/company/dropshipping-scout" target="_blank"><img src="{{ asset('images/Linkedin.svg') }}" alt=""></a></li>
-                    <li><a href="https://www.instagram.com/dropshipping.scout?igsh=bWQ0cWgwOW4zYzl5" target="_blank"><img src="{{ asset('images/Instagram.svg') }}" alt=""></a></li>
-                    <li><a href="https://youtube.com/@dropshipping.scout.?feature=shared" target="_blank"><img src="{{ asset('images/YouTube.svg') }}" alt=""></a></li>
-                    {{-- <li><a href="#"><img src="{{ asset('images/X.svg') }}" alt=""></a></li> --}}
-                    <li><a href="https://www.facebook.com/dropshipping.scout?mibextid=ZbWKwL" target="_blank"><img src="{{ asset('images/facebook.svg') }}" alt=""></a></li>
-
-                </ul>
-            </div>
-
-            <h3 style="color: #3545D6;padding-bottom: 0px;padding-top: 15px;font-weight:700" class="table-content">Table of Contents</h3>
-            <ul class="table-of-contents">
-                @foreach($headings as $heading)
-                    <li class="table-content"  >
-                        <a
-                            href="#{{ $heading['id'] }}"
-                            style="padding: 0; margin: 0; display: inline;">
-                            {{ $heading['text'] }}
-                        </a>
-                    </li>
-                @endforeach
+    @endif
+    <div class="container blog-details-container">
+        <a href="{{ route('blogs.userIndex') }}"><img src="{{ asset('images/left-arrow.svg') }}" class="back-arrow" alt="Back"></a>
+        <h1 class="title">{{ $blog->title }}</h1>
+        <div class="rowElements">
+            <button class="button-shopify">{{ $blog->category }}</button>
+            <ul class="info-list">
+                <li><span class="date">{{ $blog->publish_date }}</span></li>
+                {{-- <li><span class="author">By: {{ $blog->author }}</span></li> --}}
+                <li>
+                    <img id="" src="{{ asset('images/heart.svg') }}" alt="Likes" >
+                    <span id="like-count" class="number">{{ $blog->likes }}</span>
+                </li>
             </ul>
-
-
         </div>
-    </div>
 
- <!-- related blogs -->
- <div class="latest-news">
-    <div class="container homeBlogContainer">
-        <div class="row section-row align-items-center">
-            <div class="col-12">
-                <!-- Section Title Start -->
-                <div class="section-title">
-                    <div class="title-with-lines">
-                        <span class="line-left"></span>
-                        <h2 class="text-anime-style-3">Related blogs</h2>
-                        <span class="line-right"></span>
+
+        @if ($blog->video_url)
+            <iframe width="100%" height="415" src="https://www.youtube.com/embed/{{ \Str::after($blog->video_url, 'v=') }}" frameborder="0" allowfullscreen></iframe>
+        @else
+            <a href="{{ route('blogs.show', $blog->slug) }}">
+                <img src="{{ 'https://tsscout.com/storage/app/public/' .$blog->image }}" alt="{{ $blog->title }}">
+            </a>
+        @endif
+
+
+        <div class="content-container">
+            <div class="content">
+                {!! $blog->content !!}
+                <br>
+                like?
+                <img id="like-button" src="{{ asset('images/heart.svg') }}" alt="Likes" style="cursor: pointer;">
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <a href="{{ route('author.show', $blog->author_data->author_slug) }}"><img src="{{ $blog->author_data?->author_img ? asset('images/'.$blog->author_data->author_img) : asset('images/icon-client.svg') }}" alt="{{ $blog->author_data->author_name }}" class="img-fluid rounded"></a>
+                            </div>
+                            <div class="col-lg-9">
+                                <a href="{{ route('author.show', $blog->author_data->author_slug) }}">
+                                <h4>{{ $blog->author_data->author_name }}</h4>
+                                <p>{!! nl2br($blog->author_data->author_card) !!}</p>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <!-- Section Title End -->
+            </div>
+            <div class="sidebar">
+                <div class="social">
+                    <ul>
+                        <li><a href="https://www.tiktok.com/@dropshipping.scout" target="_blank"><img src="{{ asset('images/Tiktok.svg') }}" alt=""></a></li>
+                        {{-- <li><a href="#"><img src="{{ asset('images/Whatsapp.svg') }}" alt=""></a></li> --}}
+                        <li><a href="https://www.linkedin.com/company/dropshipping-scout" target="_blank"><img src="{{ asset('images/Linkedin.svg') }}" alt=""></a></li>
+                        <li><a href="https://www.instagram.com/dropshipping.scout?igsh=bWQ0cWgwOW4zYzl5" target="_blank"><img src="{{ asset('images/Instagram.svg') }}" alt=""></a></li>
+                        <li><a href="https://youtube.com/@dropshipping.scout.?feature=shared" target="_blank"><img src="{{ asset('images/YouTube.svg') }}" alt=""></a></li>
+                        {{-- <li><a href="#"><img src="{{ asset('images/X.svg') }}" alt=""></a></li> --}}
+                        <li><a href="https://www.facebook.com/dropshipping.scout?mibextid=ZbWKwL" target="_blank"><img src="{{ asset('images/facebook.svg') }}" alt=""></a></li>
+
+                    </ul>
+                </div>
+
+                <h3 style="color: #3545D6;padding-bottom: 0px;padding-top: 15px;font-weight:700" class="table-content">Table of Contents</h3>
+                <ul class="table-of-contents">
+                    @foreach($headings as $heading)
+                        <li class="table-content"  >
+                            <a
+                                href="#{{ $heading['id'] }}"
+                                style="padding: 0; margin: 0; display: inline;">
+                                {{ $heading['text'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+
+
             </div>
         </div>
-    </div>
-</div>
+
+        <!-- related blogs -->
+        <div class="latest-news">
+            <div class="container homeBlogContainer">
+                <div class="row section-row align-items-center">
+                    <div class="col-12">
+                        <!-- Section Title Start -->
+                        <div class="section-title">
+                            <div class="title-with-lines">
+                                <span class="line-left"></span>
+                                <h2 class="text-anime-style-3">Related blogs</h2>
+                                <span class="line-right"></span>
+                            </div>
+                        </div>
+                        <!-- Section Title End -->
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
         <div class="row" style="padding-bottom: 50px;">
             @foreach ($relatedBlogs as $blog)
-            <div class="col-lg-4 col-md-6" style="padding-bottom: 20px;">
-                <!-- Blog Item Start -->
-                <div class="blog-item wow fadeInUp" data-wow-delay="0.75s">
-                    <!-- Blog Image Start -->
-                    <div class="post-featured-image">
-                        <a href="{{ route('blogs.show', $blog->slug) }}">
-                        <figure class="image-anime">
-                                <img src="{{ ('/storage/app/public/' .$blog->image) }}" alt="{{ $blog->title }}">
-                        </figure>
-                    </a>
+                <div class="col-lg-4 col-md-6" style="padding-bottom: 20px;">
+                    <!-- Blog Item Start -->
+                    <div class="blog-item wow fadeInUp" data-wow-delay="0.75s">
+                        <!-- Blog Image Start -->
+                        <div class="post-featured-image">
+                            <a href="{{ route('blogs.show', $blog->slug) }}">
+                            <figure class="image-anime">
+                                    <img src="{{ ('/storage/app/public/' .$blog->image) }}" alt="{{ $blog->title }}">
+                            </figure>
+                        </a>
 
+                        </div>
+                        <!-- Blog Image End -->
+
+                        <!-- Blog Content Start -->
+                        <div class="post-item-body">
+
+                            <a href="{{ route('blogs.show', $blog->slug) }}" style="color: #1E3F5B">
+                                {{ $blog->created_at }}</a>
+
+                                <a href="{{ route('blogs.show', $blog->slug) }}"> <h2 class="homeBlogParagraph">{{ $blog->title }}</h2>    </a>
+                        </div>
+                        <!-- Category Label -->
+                        <div class="category-label">
+                            {{ $blog->category=='Tiktook'?'TikTok Shop':$blog->category }}
+                        </div>
+                        <!-- Blog Content End -->
                     </div>
-                    <!-- Blog Image End -->
-
-                    <!-- Blog Content Start -->
-                    <div class="post-item-body">
-
-                          <a href="{{ route('blogs.show', $blog->slug) }}" style="color: #1E3F5B">
-                            {{ $blog->created_at }}</a>
-
-                            <a href="{{ route('blogs.show', $blog->slug) }}"> <h2 class="homeBlogParagraph">{{ $blog->title }}</h2>    </a>
-                    </div>
-                    <!-- Category Label -->
-                    <div class="category-label">
-                        {{ $blog->category=='Tiktook'?'TikTok Shop':$blog->category }}
-                    </div>
-                    <!-- Blog Content End -->
+                    <!-- Blog Item End -->
                 </div>
-                <!-- Blog Item End -->
-            </div>
-        @endforeach
+            @endforeach
         </div>
 
 
 
-<!-- end of related blogs -->
+        <!-- end of related blogs -->
 
-</div>
-</div>
+    </div>
 
-<script>
-    document.getElementById('like-button').addEventListener('click', function() {
-        const blogId = {{ $blog->id }};
-        const likeButton = document.getElementById('like-button');
-        const likeCount = document.getElementById('like-count');
+    <script>
+        document.getElementById('like-button').addEventListener('click', function() {
+            const blogId = {{ $blog->id }};
+            const likeButton = document.getElementById('like-button');
+            const likeCount = document.getElementById('like-count');
 
-        alert('Thanks for the like!');
-        fetch(`/blogs/${blogId}/like`, {
-            method: 'POST',
-            headers: {
-                'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(response => response.json())
-        .then(data => {
-            likeCount.textContent = data.likes; // Update the likes count
-        })
-        .catch(error => {
-            console.error('Error:', error);
+            alert('Thanks for the like!');
+            fetch(`/blogs/${blogId}/like`, {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                likeCount.textContent = data.likes; // Update the likes count
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
         });
-    });
 
 
-    document.addEventListener('scroll', function() {
-        const sidebar = document.querySelector('.sidebar');
-        if (sidebar) {
-            const rect = sidebar.getBoundingClientRect();
-            if (rect.top === 0) {
-                sidebar.classList.add('sticky-top');
-            } else {
-                sidebar.classList.remove('sticky-top');
-            }
-        }
-    });
-
-    document.addEventListener('scroll', function() {
-        const contentHeadings = document.querySelectorAll('.content h1[id], .content h2[id], .content h3[id]');
-        const tocLinks = document.querySelectorAll('.table-of-contents a');
-
-        let activeId = null;
-
-        contentHeadings.forEach((heading) => {
-            const rect = heading.getBoundingClientRect();
-            if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
-                activeId = heading.id;
+        document.addEventListener('scroll', function() {
+            const sidebar = document.querySelector('.sidebar');
+            if (sidebar) {
+                const rect = sidebar.getBoundingClientRect();
+                if (rect.top === 0) {
+                    sidebar.classList.add('sticky-top');
+                } else {
+                    sidebar.classList.remove('sticky-top');
+                }
             }
         });
 
-        if (activeId !== null) {
-            tocLinks.forEach((link) => {
-                const href = link.getAttribute('href');
-                const isActive = href === '#' + activeId;
-                link.parentElement.classList.toggle('toc-active', isActive);
-                if (isActive) {
-                    link.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        document.addEventListener('scroll', function() {
+            const contentHeadings = document.querySelectorAll('.content h1[id], .content h2[id], .content h3[id]');
+            const tocLinks = document.querySelectorAll('.table-of-contents a');
+
+            let activeId = null;
+
+            contentHeadings.forEach((heading) => {
+                const rect = heading.getBoundingClientRect();
+                if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
+                    activeId = heading.id;
                 }
             });
-        }
-    });
-</script>
+
+            if (activeId !== null) {
+                tocLinks.forEach((link) => {
+                    const href = link.getAttribute('href');
+                    const isActive = href === '#' + activeId;
+                    link.parentElement.classList.toggle('toc-active', isActive);
+                    if (isActive) {
+                        link.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+                    }
+                });
+            }
+        });
+    </script>
 
 
 @endsection
+
+@push("schema")
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "{{ route('blogs.show', ['slug' => $blog->slug]) }}"
+        },
+        "headline": "{{ $blog->title }}",
+        "image": [
+            "{{ 'https://tsscout.com/storage/app/public/' .$blog->image }}"
+        ],
+        "author": {
+            "@type": "Person",
+            "name": "{{ $blog->author }}"
+        },
+        "publisher": {
+            "@type": "Organization",
+            "name": "TS Scout",
+            "logo": {
+                "@type": "ImageObject",
+                "url": "{{ asset('images/Scout-Logo%2020x20-03.svg') }}"
+            }
+        },
+        "datePublished": "{{ \Carbon\Carbon::parse($blog->publish_date)->toIso8601String() }}",
+        "dateModified": "{{ \Carbon\Carbon::parse($blog->updated_at)->toIso8601String() }}",
+        "description": "{{ $blog->excerpt }}"
+    }
+    </script>
+@endpush
