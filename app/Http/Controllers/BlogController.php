97822342668546c8f1eb0ab75c38d05cf3ec4f51
MagdyR <developer.eye1@gmail.com>
2026-07-12 16:18:20 +0300

    ff

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -257,7 +257,7 @@ class BlogController extends Controller
             ->where('published', true)
             ->where('scheduled_at', '<=', \Carbon\Carbon::now())
             ->orderBy('publish_date', 'desc');
-            @dd($blogsQuery->toRawSql());
+            // @dd($blogsQuery->toRawSql());
             $blogs=  $blogsQuery->get();
 
 
