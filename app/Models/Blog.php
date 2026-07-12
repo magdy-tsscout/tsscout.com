d7966940f061b7f0a443d46bf747d72698442192
MagdyR <developer.eye1@gmail.com>
2026-07-12 17:58:13 +0300

    Refactor blog model to use Auth facade for user retrieval in boot method

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class Blog extends Model
@@ -59,7 +60,7 @@ class Blog extends Model
             }
 
             if( empty($blog->author_id) ) {
-                $author = auth()->user();
+                $author = Auth::user();
                 if ($author) {
                     $blog->author_id = $author->id;
                 }
@@ -68,7 +69,7 @@ class Blog extends Model
                 $blog->scheduled_at = now();
             }
             if( isset($blog->id) ) {
-                $author = auth()->user();
+                $author = Auth::user();
                 if ($author) {
                     $blog->updated_by = $author->id;
                 }
