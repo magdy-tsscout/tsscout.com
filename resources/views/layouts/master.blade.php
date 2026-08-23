d2c4c900a19cb5961a8ed60476d8772ff71bf758
MagdyR <developer.eye1@gmail.com>
2026-08-23 17:18:21 +0300

    Remove authentication parameters from Google Tag Manager script for security compliance

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -68,7 +68,7 @@
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=Bi_cHgKPGrX8rjoUm5oFcQ&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KV3N43LJ');</script>
 <!-- End Google Tag Manager -->
 </head>
