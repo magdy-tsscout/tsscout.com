3c4c25dca8c4e0d1e342918e7a8362a5bdbfc8e6
MagdyR <developer.eye1@gmail.com>
2026-09-23 13:01:22 +0300

    Add root route for sitemap generation in web.php for improved accessibility

--- routes/web.php
+++ routes/web.php
@@ -124,8 +124,9 @@ Route::get('author/{slug}', [AuthorDataController::class, 'show'])->name('author
 
 // sitemap
 Route::group(['prefix' => 'sitemap'], function () {
+    Route::get('/', [SiteMapController::class, 'generate'])->name('sitemap.root');
     Route::get('blog-sitemap.xml', [BlogController::class, 'sitemap'])->name('pages.sitemap'); // Pages sitemap
-    Route::get('/sitemap.xml', [SiteMapController::class, 'generate'])->name('sitemap.generate');
+    Route::get('sitemap.xml', [SiteMapController::class, 'generate'])->name('sitemap.generate');
 });
 
 
